2f086ff24520f362e8c1e83268ce3bf5d8af6425
fadd00 <andhikahutama9@gmail.com>
2025-11-27 13:04:31 +0700

    feat: Introduce DetailSiswa data class and conversion functions for student and UI state objects.

--- app/src/main/java/com/sample/prak8/viewmodel/entryViewModel.kt
+++ app/src/main/java/com/sample/prak8/viewmodel/entryViewModel.kt
@@ -25,3 +25,22 @@ data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
